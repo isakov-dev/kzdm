91af67a9cc0a56906c521086c200f07daabf0712
Artur Meshchenkov <artur133715@gmail.com>
2021-05-04 16:02:16 +0600

    modif: добавление нового города

--- local/templates/.default/components/bitrix/news.list/federals-kz/lang/ru/template.php
+++ local/templates/.default/components/bitrix/news.list/federals-kz/lang/ru/template.php
@@ -3,3 +3,4 @@ $MESS['PRESENT'] = 'Представительства в Казахстане';
 $MESS['REGIONS_1'] = 'Нур-Султан';
 $MESS['REGIONS_2'] = 'Алматы';
 $MESS['REGIONS_3'] = 'Караганда';
+$MESS['REGIONS_4'] = 'Шымкент';
