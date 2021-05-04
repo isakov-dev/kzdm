d651df90dd2582e0bfe14eb398dd2a5119047c9e
Artur Meshchenkov <artur133715@gmail.com>
2021-05-04 15:59:05 +0600

    modif: запрет подраздела для кз(404)

--- local/templates/.default/components/bitrix/news.detail/services.detail/template.php
+++ local/templates/.default/components/bitrix/news.detail/services.detail/template.php
@@ -1,6 +1,14 @@
 <?php
 $this->setFrameMode(true);
 ?>
+<?php
+if($arResult['ID'] == 79 && DOMAIN_ZONE == 'kz') {
+    CHTTP::SetStatus("404 Not Found");
+    @define("ERROR_404","Y");
+    require(\Bitrix\Main\Application::getDocumentRoot() . "/404.php");
+    die();
+}
+?>
 <div class="single">
     <?php if (!empty($arResult)): ?>
         <?php if (!empty($arResult['DETAIL_PICTURE'])): ?>
@@ -83,6 +91,77 @@ $this->setFrameMode(true);
         <?php if (!empty($arResult['DETAIL_TEXT'])): ?>
             <?= $arResult['DETAIL_TEXT']; ?>
         <?php endif; ?>
+                <?php
+                    if(DOMAIN_ZONE == 'kz' && ($arResult['ID'] == 81 || $arResult['ID'] == 78 || $arResult['ID'] == 80)) {
+                ?>
+                    <div class="wrapper wrapper_default">
+                    <? $APPLICATION->IncludeComponent(
+                        "bitrix:news.list",
+                        (DOMAIN_ZONE == 'kz' ? "federals-kz" : "federals"),
+                        array(
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "CACHE_FILTER" => "N",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "Y",
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "FIELD_CODE" => array("", ""),
+                            "FILTER_NAME" => "",
+                            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                            "IBLOCK_ID" => (DOMAIN_ZONE == 'kz' ? "61" : "17"),
+                            "IBLOCK_TYPE" => (DOMAIN_ZONE == 'regions_kz' ? "61" : "regions"),
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                            "INCLUDE_SUBSECTIONS" => "Y",
+                            "MESSAGE_404" => "",
+                            "NEWS_COUNT" => "20",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_DESC_NUMBERING" => "N",
+                            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_SHOW_ALWAYS" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "PAGER_TITLE" => "Новости",
+                            "PARENT_SECTION" => "",
+                            "PARENT_SECTION_CODE" => "",
+                            "PREVIEW_TRUNCATE_LEN" => "",
+                            "PROPERTY_CODE" => array("ADDRESS", "PHONE", 'EMAIL'),
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_STATUS_404" => "N",
+                            "SET_TITLE" => "N",
+                            "SHOW_404" => "N",
+                            "SORT_BY1" => "SORT",
+                            "SORT_BY2" => "ACTIVE_FROM",
+                            "SORT_ORDER1" => "ASC",
+                            "SORT_ORDER2" => "DESC",
+                            "STRICT_SECTION_CHECK" => "N"
+                        )
+                    ); ?>
+                    </div>
+                <?php } ?>
     <?php endif; ?>
+    <?php
+        if(DOMAIN_ZONE == 'kz') { ?>
+            <style>
+                .kz-display-none {
+                    display: none;
+                }
+            </style>
+        <?php } ?>
     <?php $APPLICATION->IncludeFile(SITE_TEMPLATE_PATH . '/include/blueform.php'); ?>
 </div>
