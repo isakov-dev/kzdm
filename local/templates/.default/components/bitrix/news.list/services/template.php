48140e1aea38b8e2d1f70f619624b2fc6c5dbfb5
Artur Meshchenkov <artur133715@gmail.com>
2021-05-04 16:02:42 +0600

    modif: запрет вывода лизинга для кз

--- local/templates/.default/components/bitrix/news.list/services/template.php
+++ local/templates/.default/components/bitrix/news.list/services/template.php
@@ -8,6 +8,9 @@ $this->setFrameMode(true);
                 <div class="row">
                     <div class="col col--xl-8 col--xl-offset-2">
                         <?php foreach ($arResult['ITEMS'] as $key => $value): ?>
+                            <?php
+                                if($value['ID'] != 79 && DOMAIN_ZONE == 'kz') {
+                            ?>
                             <a href="<?= $value['DETAIL_PAGE_URL']; ?>" class="service-tile service-tile_offset">
                                 <span class="service-tile__content">
                                     <span class="service-tile__title h3 title title_block title_small-offset"><?= $value['NAME']; ?></span>
@@ -24,6 +27,24 @@ $this->setFrameMode(true);
                                     </svg>
                                 </span>
                             </a>
+                            <?php } else if(DOMAIN_ZONE != 'kz') { ?>
+                                <a href="<?= $value['DETAIL_PAGE_URL']; ?>" class="service-tile service-tile_offset">
+                            <span class="service-tile__content">
+                                <span class="service-tile__title h3 title title_block title_small-offset"><?= $value['NAME']; ?></span>
+                                <?php if (!empty($value['PREVIEW_TEXT'])): ?>
+                                    <span class="service-tile__desc"><?= $value['PREVIEW_TEXT']; ?></span>
+                                <?php endif; ?>
+                            </span>
+                                    <span class="circle-arrow">
+                                <svg height="124" width="124" class="circle-arrow__line">
+                                    <circle cx="60" cy="60" r="59" stroke-width="1" fill="none" />
+                                </svg>
+                                <svg class="circle-arrow__ico">
+                                    <use xlink:href="<?= SITE_DEFAULT_TEMPLATE_PATH; ?>/assets/images/icons.svg#arrow-r-slim"></use>
+                                </svg>
+                            </span>
+                                </a>
+                            <?php } ?>
                         <?php endforeach; ?>
                     </div>
                 </div>
