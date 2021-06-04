bd6f0e057b781df9d4c18a1e3762d96b63bfa5a1
Ponika38 <leha140120022@gmail.com>
2021-06-04 13:22:31 +0600

    Fix: смена текста кнопки

--- local/templates/.default/components/bitrix/news.list/homepage.header.video/lang/en/template.php
+++ local/templates/.default/components/bitrix/news.list/homepage.header.video/lang/en/template.php
@@ -1,4 +1,4 @@
 <?php
-$MESS["CATALOG_BUTTON"] = "Technology Catalog";
-$MESS["TECHNICS_IN_STOCK"] = "Technology in stock";
-$MESS["LEASING_CALC"] = "Leasing calculator";
+$MESS["CATALOG_BUTTON"] = "Products";
+$MESS["TECHNICS_IN_STOCK"] = "On stock";
+$MESS["LEASING_CALC"] = "Leasing calculator";    
